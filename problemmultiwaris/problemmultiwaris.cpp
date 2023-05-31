418fe69698c0f294bdcff524c50b12c00011ce3b
andiharun29 <andiharunalrasyid14@gmail.com>
2023-05-31 10:59:43 +0700

    Add project files.

---
+++ problemmultiwaris/problemmultiwaris.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std std;
+
+class orang {
+public:
+	int umur;
+
+	orang(int pumur) :
+		umur(pumur)
+	{
+		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
