0fd8bc72d53d77135ca29250dbff9c461c79a76c
andiharun29 <andiharunalrasyid14@gmail.com>
2023-05-31 13:13:53 +0700

    membuat class drivedclass ngna menambahkan pewarisan dari class baseclass

--- program NOoverriding/program NOoverriding.cpp
+++ program NOoverriding/program NOoverriding.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class baseclasss {
+class baseclass {
 public:
 	virtual void perkenalan() final {
 		cout << "halo saya function dari base class";
+	}
+};
+
+class derivedclass : public baseclass {
+public:
+	void perkenalan() {
+		cout << " halo saya function dari derived class";
 	}
 };
