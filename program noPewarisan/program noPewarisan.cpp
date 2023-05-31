5b5d72747b0bfa3d714c86ae93982e7229876e73
andiharun29 <andiharunalrasyid14@gmail.com>
2023-05-31 15:45:30 +0700

    membuat clas derived tanpa mewariskan ke class baseclass

--- program noPewarisan/program noPewarisan.cpp
+++ program noPewarisan/program noPewarisan.cpp
@@ -7,3 +7,9 @@ public:
 		cout << "halo saya function dari base class";
 	}
 };
+class derivedclass : public baseclass {
+public:
+	void perkenalan() {
+		cout << "halo saya function dari devired class";
+	}
+};
