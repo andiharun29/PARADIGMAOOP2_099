967baad958542e49a6e2e599b461a8e74c2578af
andiharun29 <andiharunalrasyid14@gmail.com>
2023-05-31 12:54:24 +0700

    membuat class seseorang dengan virtual pesan dan virtual cetak

---
+++ polymorphism/polymorphism.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+	virtual void pesan() = 0;
+	virtual void pesan() {
+		cout << "pesan dari seseorang" << endl;
+	}
+	virtual void cetak() = 0;
+	virtual void cetak() {
+
+	}
+};
